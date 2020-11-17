9844c45ac046c19fff3d66a0dd964cf6c3969253
Elisabeth-A-C <69512985+Elisabeth-A-C@users.noreply.github.com>
2020-11-17 12:20:51 +0100

    Organisering

--- Main/NewTest.js
+++ Main/NewTest.js
@@ -1,5 +1,5 @@
 function createDatabase() {
-   const dbName = "the_name";
+  const dbName = "the_name";
 
   var request = indexedDB.open(dbName, 2);
 
@@ -89,57 +89,51 @@ function showQuestion () {
 }
 
 
-function GenerateQuestion () {
+function GenerateNewQuestion () {
+  var container = document.getElementById('container');
 
-  document.getElementById('submit').onclick = function() {
+  //Show question
+  var question = document.createElement('h2');
+  question.innerHTML = "Nyt spørgsmål";
+  container.appendChild(question);
 
-    var container = document.getElementById('container');
+  var questionTxtField = document.createElement('input');
+  questionTxtField.type = 'text';
+  questionTxtField.id = 'a';
+  questionTxtField.value = 'Skriv spørgsmål her';
+  questionTxtField.name = 'question';
+  questionTxtField.style.fontSize = "large";
+  container.appendChild(questionTxtField);
+  insertNewline(2);
 
-    //Show question
-    var question = document.createElement('h2');
-    question.innerHTML = "Nyt spørgsmål";
-    container.appendChild(question);
+  //Generate textfields for questions
+  for (var i = 1; i <= 4; i++)
+  {
+    var radiobox = document.createElement('input');
 
-    var questionTxtField = document.createElement('input');
-    questionTxtField.type = 'text';
-    questionTxtField.id = 'a'+i;
-    questionTxtField.value = 'Skriv spørgsmål her';
-    questionTxtField.name = 'question';
-    questionTxtField.style.fontSize = "large";
-    container.appendChild(questionTxtField);
+    radiobox.type = 'radio';
+    radiobox.id = 'rightAnswer'+i;
+    radiobox.value = i;
+    radiobox.name = 'answer'+question_no;
+
+    container.appendChild(radiobox);
+
+    var answerTxt = document.createElement('text');
+    answerTxt.innerHTML = 'Answer ' + i;
+    container.appendChild(answerTxt);
+
+    insertNewline (1);
+
+    var answerTxtField = document.createElement('input');
+    answerTxtField.type = 'text';
+    answerTxtField.id = 'a'+i;
+    answerTxtField.name = 'answer';
+    container.appendChild(answerTxtField);
+
     insertNewline(2);
+  }
 
-    //Generate textfields for questions
-    for (var i = 1; i <= 4; i++)
-    {
-      var radiobox1 = document.createElement('input');
-
-      radiobox1.type = 'radio';
-      radiobox1.id = 'rightAnswer'+i;
-      radiobox1.value = i;
-      radiobox1.name = 'answer'+question_no;
-
-      container.appendChild(radiobox1);
-
-      var answerTxt = document.createElement('text');
-      answerTxt.innerHTML = 'Answer ' + i;
-      container.appendChild(answerTxt);
-      //var answerTxtnode = document.createTextNode('Answer ' + i);
-
-      var newline = document.createElement('br');
-      container.appendChild(newline);
-
-      var answerTxtField = document.createElement('input');
-      answerTxtField.type = 'text';
-      answerTxtField.id = 'a'+i;
-      answerTxtField.name = 'answer';
-      container.appendChild(answerTxtField);
-
-      insertNewline(2);
-    }
-
-    question_no = question_no + 1;
-  };
+  question_no = question_no + 1;
 }
 
 function insertNewline(no)
