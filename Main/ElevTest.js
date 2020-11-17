61dd363dc1a47ae551ec8c36c71c0976f5fe9018
Picomus <70012057+Picomus@users.noreply.github.com>
2020-11-17 12:26:48 +0100

    kode til login mm
    fskjgf,sje

--- Main/ElevTest.js
+++ Main/ElevTest.js
@@ -1,36 +1,12 @@
-function createDatabase() {
-
-  var container = document.getElementById('container');
-
-  try {
-    //if (window.openDatabase) {
-    var shortName = 'testFest.db';
-    var version = '1.0';
-    var displayName = 'Display Information';
-    var maxSize = 65536; // in bytes
-    db = openDatabase(shortName, version, displayName, maxSize);
-    // }
-    var labelx = document.createElement('label');
-    labelx.htmlFor = 'labelx';
-    var descriptionx = document.createTextNode('Yo');
-    labelx.appendChild(descriptionx);
-    container.appendChild(labelx);
-  }
-  catch(e) {
-
-    alert(e);
-  }
-}
 
 function showQuestion () {
   document.getElementById('submit').onclick = function() {
-
 
     var container = document.getElementById('container');
 
     //Show question
     var question = document.createElement('h2');
-    question.innerHTML = "Question";
+    question.innerHTML = "Spørgsmål";
     container.appendChild(question);
 
 
@@ -44,7 +20,7 @@ function showQuestion () {
       radiobox1.id = i;
       radiobox1.value = i;
       radiobox1.name = 'question';
-  
+
       var label1 = document.createElement('label');
       label1.htmlFor = 'Answer ' + i;
 
