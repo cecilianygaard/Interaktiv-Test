b86207fc95931c0c7d756e8adb9d815f3cd3c2d7
Picomus <70012057+Picomus@users.noreply.github.com>
2020-11-27 12:13:50 +0100

    manual merge 2.0
    mergefejl....

--- Main/database_user.js
+++ Main/database_user.js
@@ -18,7 +18,6 @@ const TEACHER = "1";
 const STUDENT = "2";
 
 //prefixes of implementation that we want to test
-<<<<<<< Updated upstream
          window.indexedDB = window.indexedDB || window.mozIndexedDB || 
          window.webkitIndexedDB || window.msIndexedDB;
          
@@ -29,15 +28,15 @@ const STUDENT = "2";
          
          if (!window.indexedDB) {
             window.alert("Your browser doesn't support a stable version of IndexedDB.");
-         }
+         }     
          
          //dette er 2 testbrugere  
          const userData = [
-            { brugerid: TEACHER, login: "p", password: "", email: "abc@def.com", name: "pernille"},
-            { brugerid: STUDENT, login: "dude", password: "ho", email: "du@de.com", name: "nilleper"}
+            { brugerid: TEACHER, login: "p", password: "47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=", email: "abc@def.com", name: "Pernille"},
+            { brugerid: STUDENT, login: "dude", password: "qCHGLoEE+FGdY5tMCUiuzmQbFD9mAfoUWZO7LixymdQ=", email: "du@de.com", name: "Nilleper"}
             //hardcode more users here
          ];
-         
+                
           const dummyanswerData = [
             {text: "svar"}
          ];
@@ -114,80 +113,73 @@ const STUDENT = "2";
                objectStoreTest.add(dummyTestData[l]);
             }
             
-         };
+         }; 
           
-         function check_password()
-         {
-           
-           //retrieve username and password from login page
-           var brugernavn = document.getElementById('user_login_textfield').value;
-           var pw = document.getElementById('user_password_textfield').value;
-           
-           //point to user table in db
-            var objectStore = db.transaction("user").objectStore("user");
+async function check_password() {
+  //retrieve username and password from login page
+      var brugernavn = document.getElementById('user_login_textfield').value;
+        var pw = document.getElementById('user_password_textfield').value;
+        var hashedpw = await asyncHash(pw);
+
+ //point to user table in db
+        var objectStore = db.transaction("user").objectStore("user");
      
             //search login
-            var index = objectStore.index("login");
+        var index = objectStore.index("login");
 
-            //do the search
-            index.get(brugernavn).onsuccess = function(event) {
-               //user found, now check if password is correct
-               if (event.target.result.password == pw) {
-                 sessionStorage.username = event.target.result.name;
-                 sessionStorage.brugerid = event.target.result.brugerid;
-                 if (event.target.result.brugerid == TEACHER)
-                 {
-                    window.location.href = 'Mail_laerer.html';    
-                 }
-                 else if (event.target.result.brugerid == STUDENT)
-                 {
-                    window.location.href = 'Main_elev.html';
-                 }      
-               }
-               else
-               {
-                 alert("Password wrong");
-               }
-            };
+  //do the search
+  index.get(brugernavn).onsuccess = function(event) {
+    //user found, now check if password is correct
+    if (event.target.result.password == hashedpw) {
+      sessionStorage.username = event.target.result.name;
+      sessionStorage.brugerid = event.target.result.brugerid;
+      if (event.target.result.brugerid == TEACHER)
+      {
+        window.location.href = 'Main_laerer.html';
+      } else if (event.target.result.brugerid == STUDENT)
+      {
+        window.location.href = 'Main_elev.html';
+      }
+    } else
+    {
+      alert("Password wrong");
+    }
+  };
 
-            index.get(brugernavn).onerror = function(event) {
-               alert("user not found");
-            };
-         }
+  index.get(brugernavn).onerror = function(event) {
+    alert("user not found");
+  };
+}
+
+   function getTest() {
        
-         
-         function getTest() {
-       
-            var objectStore = db.transaction(["test"], "readwrite")
-            .objectStore("test");
+     var objectStore = db.transaction(["test"], "readwrite")
+        .objectStore("test");
           
-             
-            var request = objectStore.get(2);
+     var request = objectStore.get(2);
          
             
-            
-            request.onerror = function(event) {
-               alert("Unable to retrieve data from database!");
+         request.onerror = function(event) {
+           alert("Unable to retrieve data from database!");
             };
             
-            request.onsuccess = function(event) {
-               // Do something with the request.result!
-               if(request.result) {
-                  //alert("Name: " + request.result.testname + ", Questions: " + request.result.questions + ", Username: " + request.result.username);
+         request.onsuccess = function(event) {
+         // Do something with the request.result!
+           if(request.result) {
+            //alert("Name: " + request.result.testname + ", Questions: " + request.result.questions + ", Username: " + request.result.username);
                   
-                  //get question keys
-                  var temp_str = request.result.questions;
-                  var questions_keys = temp_str.split("|");
+             //get question keys
+             var temp_str = request.result.questions;
+             var questions_keys = temp_str.split("|");
                 
-                  showQuestions(questions_keys);
-               } else {
-                  alert("Question couldn't be found in your database!");
+          showQuestions(questions_keys);
+            } else {
+              alert("Question couldn't be found in your database!");
                }
             };
          }
          
-function showQuestions(questions_keys)
-{
+function showQuestions(questions_keys){
    var container = document.getElementById('container');
    
    //Show question
@@ -196,8 +188,7 @@ function showQuestions(questions_keys)
    //container.appendChild(question);
    
    //iterate through questions
-   for (var j = 0; j < questions_keys.length; j++)
-   {
+   for (var j = 0; j < questions_keys.length; j++) {
        let objectStore = db.transaction(["questions"], "readwrite").objectStore("questions");
        let request = objectStore.get(parseInt(questions_keys[j]));
        
@@ -218,7 +209,6 @@ function showQuestions(questions_keys)
        request.onerror = function(event) {
      
        };
-       
    }
    
    //below are test functions that might come in handy later
@@ -286,251 +276,3 @@ function showQuestions(questions_keys)
             };
          }  
 */
-=======
-window.indexedDB = window.indexedDB || window.mozIndexedDB || 
-  window.webkitIndexedDB || window.msIndexedDB;
-
-//prefixes of window.IDB objects
-window.IDBTransaction = window.IDBTransaction || 
-  window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-
-if (!window.indexedDB) {
-  window.alert("Your browser doesn't support a stable version of IndexedDB.");
-}
-
-const userData = [
-{ 
-brugerid: 
-TEACHER, username: 
-"p", password: 
-"47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=", email: 
-  "abc@def.com"
-}
-, 
-{ 
-brugerid: 
-STUDENT, username: 
-"dude", password: 
-"qCHGLoEE+FGdY5tMCUiuzmQbFD9mAfoUWZO7LixymdQ=", email: 
-  "du@de.com"
-}
-//hardcode more users here
-];
-
-const dummyanswerData = [
-{
-text: 
-  "svar"
-}
-];
-
-const dummyQuestionData = [
-{
-text: 
-"Spørgsmål", answers:
-"", correctAnswers: 
-  ""
-}
-];
-
-const dummyTestData = [
-{
-testname: 
-"Testxxx", questions:
-"", username: 
-  ""
-}
-];
-
-var db;
-var request = window.indexedDB.open("newDatabase", 1);
-
-request.onerror = function(event) {
-  console.log("error: ");
-};
-
-request.onsuccess = function(event) {
-  db = request.result;
-  console.log("success: " + db);
-};
-
-//this function will create the database if it doesn't exist
-request.onupgradeneeded = function(event) {
-  var db = event.target.result;
-
-
-  /* Generate user table in database */
-
-
-  //create user table and tell database that key is username (unique)
-  var objectStore = db.createObjectStore("user", {
-  keyPath: 
-    "username"
-  }
-  );
-
-  //needed for index based searching by username
-  objectStore.createIndex("username", "username", { 
-  unique: 
-    true
-  }
-  );
-
-  //populate database with hardcoded entries
-  for (var i in userData) {
-    objectStore.add(userData[i]);
-  }
-
-  /* Generate answer table in database */
-
-  var objectStoreAnswers = db.createObjectStore("answers", { 
-  autoIncrement : 
-    true
-  }
-  );
-
-  //needed for index based searching by id
-  //objectStoreAnswers.createIndex("id", "id", { unique: true });
-
-  //populate database with hardcoded entries
-  for (var j in dummyanswerData) {
-    objectStoreAnswers.add(dummyanswerData[j]);
-  }
-
-  /* Generate question table */
-
-  var objectStoreQuestions = db.createObjectStore("questions", { 
-  autoIncrement : 
-    true
-  }
-  );
-
-  //needed for index based searching by id
-  //objectStoreAnswers.createIndex("id", "id", { unique: true });
-
-  //populate database with hardcoded entries
-  for (var k in dummyQuestionData) {
-    objectStoreQuestions.add(dummyQuestionData[k]);
-  }
-
-  /* Generate question table */
-  var objectStoreTest = db.createObjectStore("test", { 
-  autoIncrement : 
-    true
-  }
-  );
-
-  //needed for index based searching by id
-  //objectStoreAnswers.createIndex("id", "id", { unique: true });
-
-  //populate database with hardcoded entries
-  for (var l in dummyTestData) {
-    objectStoreTest.add(dummyTestData[l]);
-  }
-}
-
-async function check_password() {
-  //retrieve username and password from login page
-  var brugernavn = document.getElementById('Brugernavn').value;
-  var pw = document.getElementById('Adgangskode').value;
-  var hashedpw = await asyncHash(pw);
-
-  //point to user table in db
-  var objectStore = db.transaction("user").objectStore("user");
-
-  //search in username
-  var index = objectStore.index("username");
-
-  //do the search
-  index.get(brugernavn).onsuccess = function(event) {
-    //user found, now check if password is correct
-    if (event.target.result.password == hashedpw) {
-      sessionStorage.username = event.target.result.username;
-      sessionStorage.brugerid = event.target.result.brugerid;
-      if (event.target.result.brugerid == TEACHER)
-      {
-        window.location.href = 'Mail_laerer.html';
-      } else if (event.target.result.brugerid == STUDENT)
-      {
-        window.location.href = 'Main_elev.html';
-      }
-    } else
-    {
-      alert("Password wrong");
-    }
-  };
-
-  index.get(brugernavn).onerror = function(event) {
-    alert("user not found");
-  };
-}
-
-
-//below are test functions that might come in handy later
-
-function read() {
-  var transaction = db.transaction(["user"]);
-  var objectStore = transaction.objectStore("user");
-  var request = objectStore.get("Svend");
-
-  request.onerror = function(event) {
-    alert("Unable to retrieve data from database!");
-  };
-
-  request.onsuccess = function(event) {
-    // Do something with the request.result!
-    if (request.result) {
-      alert("Name: " + request.result.username + ", Password: " + request.result.password + ", Email: " + request.result.email);
-    } else {
-      alert("Svend couldn't be found in your database!");
-    }
-  };
-}
-
-function read_all_users() {
-  var objectStore = db.transaction("user").objectStore("user");
-
-  objectStore.openCursor().onsuccess = function(event) {
-    var cursor = event.target.result;
-
-    if (cursor) {
-      alert("Username: " + cursor.key + ", Id: " + cursor.value.brugerid + ", Password: " + cursor.value.password + ", Email: " + cursor.value.email);
-      cursor.continue();
-    } else {
-      alert("No more entries!");
-    }
-  };
-}
-
-function add_user() {
-  var request = db.transaction(["user"], "readwrite")
-    .objectStore("user")
-    .add( { 
-  brugerid: 
-  "2", username: 
-  "Svend", password: 
-  "hej", email: 
-    "svend@tveskaeg.org"
-  }
-  );
-
-  request.onsuccess = function(event) {
-    alert("Svend has been added to your database.");
-  };
-
-  request.onerror = function(event) {
-    alert("Unable to add data\r\nSvend is aready exist in your database! ");
-  }
-}
-
-function remove_user() {
-  var request = db.transaction(["user"], "readwrite")
-    .objectStore("user")
-    .delete("Svend");
-
-  request.onsuccess = function(event) {
-    alert("Svend's entry has been removed from your database.");
-  };
-}      
->>>>>>> Stashed changes
