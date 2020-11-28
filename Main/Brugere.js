c03b956df9f4590cd26b48542679b8179688d70b
Elisabeth-A-C <69512985+Elisabeth-A-C@users.noreply.github.com>
2020-11-28 22:16:20 +0100

    Hashed password fixed in "Brugere"

--- Main/Brugere.js
+++ Main/Brugere.js
@@ -1,15 +1,16 @@
 //opret bruger kode:
 
-function add_user() {
+async function add_user() {
   
   var name = document.getElementById('user_name_textfield').value;
   var login = document.getElementById('user_login_textfield').value;
   var email = document.getElementById('user_email_textfield').value;
   var password = document.getElementById('user_password_textfield').value;
+  var hashedpw = await asyncHash(password);
     
   var request = db.transaction(["user"], "readwrite")
     .objectStore("user")
-    .add( { brugerid: STUDENT, login: login, password: password, email: email, name: name});
+    .add( { brugerid: STUDENT, login: login, password: hashedpw, email: email, name: name});
 
   request.onsuccess = function(event) {
     alert("user has been added to your database.");
