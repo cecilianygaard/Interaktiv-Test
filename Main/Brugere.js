a6f397d9ddf6cb11df586fcc4877055e8e88d7b7
Elisabeth-A-C <69512985+Elisabeth-A-C@users.noreply.github.com>
2020-11-29 11:52:07 +0100

    Opret brugere done!

--- Main/Brugere.js
+++ Main/Brugere.js
@@ -1,22 +1,40 @@
 //opret bruger kode:
 
-async function add_user() {
+function add_user() {
+  var team = document.getElementById('team_name_textfield').value;
   
-  var name = document.getElementById('user_name_textfield').value;
-  var login = document.getElementById('user_login_textfield').value;
-  var email = document.getElementById('user_email_textfield').value;
-  var password = document.getElementById('user_password_textfield').value;
-  var hashedpw = await asyncHash(password);
+  //Check if team exists
+  var teamObjectStore = db.transaction(["team"], "readonly")
+  .objectStore("team");
+  var teamRequest = teamObjectStore.get(team);
+  
+  teamRequest.onerror = function(event) {
+      alert("Team " + team + " eksisterer ikke");
+  }
+  
+  teamRequest.onsuccess = async function(event) {
+    if (teamRequest.result==null) {
+      alert("Team " + team + " eksisterer ikke");
+      return;
+    }
+  
+    //The team did exist
+    var name = document.getElementById('user_name_textfield').value;
+    var login = document.getElementById('user_login_textfield').value;
+    var email = document.getElementById('user_email_textfield').value;
+    var password = document.getElementById('user_password_textfield').value;
+    var hashedpw = await asyncHash(password);
     
-  var request = db.transaction(["user"], "readwrite")
-    .objectStore("user")
-    .add( { brugerid: STUDENT, login: login, password: hashedpw, email: email, name: name});
+    var request = db.transaction(["user"], "readwrite")
+      .objectStore("user")
+      .add( { brugerid: STUDENT, login: login, password: hashedpw, email: email, name: name, team: team });
 
-  request.onsuccess = function(event) {
-    alert("user has been added to your database.");
-  };
+    request.onsuccess = function(event) {
+      alert("user has been added to your database.");
+    };
 
-  request.onerror = function(event) {
-    alert("Unable to add data\r\n user already exists in your database ");
-  };
+    request.onerror = function(event) {
+      alert("User already exists in your database.");
+    };  
+  }
 }
